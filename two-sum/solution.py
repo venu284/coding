cc759dd32e9075f945c42599e70b3d46b73edc35
venu  dattathreya vemuru <venudattathreya@gmail.com>
2026-06-20 23:27:04 -0400

    Refactor twoSum method to improve variable naming and simplify logic

--- two-sum/solution.py
+++ two-sum/solution.py
@@ -3,5 +3,9 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen: dict[int, int] = {}
-
-        
+        map = {}
+        for i,n in enumerate(nums):
+            diff = target-n
+            if diff in map:
+                return (map[diff], i)
+            map[n] = i
